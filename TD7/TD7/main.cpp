6036076de8b269bcf0856363efd726fc5363b2e1
NicolasGH <nicogheung@gmail.com>
2015-11-12 21:32:46 +0100

    Initial Commit

---
+++ TD7/TD7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TD7
+//
+//  Created by Nicolas GHEUNG on 12/11/15.
+//  Copyright © 2015 Nicolas GHEUNG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
